ae163d2e02b005ef56dc1d48e019fe35189ee511
Bruno Signori <signoribruno@gmail.com>
2023-02-19 17:44:04 -0500

    changes to dataabase class

--- src/main/java/Emp_Connector/SFDC_Events/DatabaseService.java
+++ src/main/java/Emp_Connector/SFDC_Events/DatabaseService.java
@@ -2,19 +2,16 @@ package Emp_Connector.SFDC_Events;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.service.ServiceRegistry;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 
 
 public class DatabaseService {
-	
+
 	
 	public void dataProcess(String key,Object val) {
 			
@@ -24,49 +21,34 @@ public class DatabaseService {
 				map.forEach((k,v) -> System.out.println("key : "+k+" value : "+v));
 			}
 			if(!map.isEmpty()) {
-				dbTransaction(map);
+				getCurrentSession(map);
 			}
 			
 	}
 	
-	public void dbTransaction(HashMap<String,String> map) {
+	public void getCurrentSession(HashMap<String,String> map) {
 		
-		System.out.println("in dbc");	
-		try {
-			Session session = getCurrentSession();
-			session.beginTransaction();
-			GuestUser user = new GuestUser(map.get("App__c"),map.get("UserIP__c"),map.get("Image_Type__c"),map.get("LoginType__c"),map.get("CreatedById"));
-			session.persist(user);
-			session.getTransaction().commit();
-			//session.close();
-			session.disconnect();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}	
-	}
-	
-	public static Session getCurrentSession() {
 		List<String> dbproperties = ServiceCredential.dbConnection();
-		Map<String, String> settings = new HashMap<>();
-		settings.put("connection.driver_class", "org.postgresql.Driver");
-		settings.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-		settings.put("hibernate.connection.url",dbproperties.get(2));
-		settings.put("hibernate.connection.username",dbproperties.get(0));
-		settings.put("hibernate.connection.password",dbproperties.get(1));
-		settings.put("hibernate.current_session_context_class", "thread");
-		settings.put("hibernate.show_sql", "true");
-		settings.put("hibernate.format_sql", "true");
-
-		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-		                                    .applySettings(settings).build();
-
-		MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-		metadataSources.addAnnotatedClass(GuestUser.class);
-		Metadata metadata = metadataSources.buildMetadata();
-		SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-		Session session = sessionFactory.openSession();
 		
-		return session;
-		
+		Configuration configuration = new Configuration();
+		configuration.setProperty("connection.driver_class", "org.postgresql.Driver");
+		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		configuration.setProperty("hibernate.connection.url",dbproperties.get(2));
+		configuration.setProperty("hibernate.connection.username",dbproperties.get(0));
+		configuration.setProperty("hibernate.connection.password",dbproperties.get(1));
+		configuration.setProperty("hibernate.current_session_context_class", "thread");
+		configuration.setProperty("hibernate.connection.pool_size","1");
+		configuration.setProperty("hibernate.show_sql", "true");
+		configuration.setProperty("hibernate.format_sql", "true");
+		configuration.addAnnotatedClass(GuestUser.class);
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		Transaction tx = session.beginTransaction();
+		GuestUser user = new GuestUser(map.get("App__c"),map.get("UserIP__c"),map.get("Image_Type__c"),map.get("LoginType__c"),map.get("CreatedById"));
+		session.persist(user);
+		tx.commit();
+		session.close();
+		sessionFactory.close();	
 		}
+
 }
